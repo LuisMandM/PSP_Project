a221cb39b8256912d22a03cb3552367a4e6c23b8
LuisMandM <luisenrique.medina@ikasle.egibide.org>
2024-12-03 14:01:16 +0100

    Add V_Login

---
+++ src/main/java/GUI/V_Login.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class V_Login {
+    private JTextField userTxtFld;
+    private JTextField passwordTxtFld;
+    private JButton iniciarSesiónButton;
+    private JButton registrarseButton;
+    private JPanel panelPrincipal;
+}
