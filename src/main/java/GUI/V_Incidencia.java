872b863c96a5f0a26e7790e0581b3c623c27b125
LuisMandM <luisenrique.medina@ikasle.egibide.org>
2024-12-03 21:56:31 +0100

    View Incidencia

---
+++ src/main/java/GUI/V_Incidencia.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+
+public class V_Incidencia {
+    private JPanel panelPrincipal;
+    private JTabbedPane tabbedPane1;
+    private JComboBox comboBox1;
+    private JTextArea textArea1;
+    private JButton enviarButton;
+    private JButton limpiarButton;
+    private JTextField areaView;
+    private JTextArea descView;
+    private JTextField estadoView;
+    private JButton verIncidenciaButton;
+    private JScrollPane incidenciasScrollp;
+}
