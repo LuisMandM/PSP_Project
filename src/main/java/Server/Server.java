80b4c203e1334feebf7bba67b921a045b2d88e59
LuisMandM <luisenrique.medina@ikasle.egibide.org>
2024-12-03 21:32:19 +0100

    Primer Metodo del Server Implementado

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,4 +1,35 @@
 package Server;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+
 public class Server {
+
+    public static int PORT = 8888;
+
+    public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.keyStore", ".\\Certificados\\AlmacenSSL.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "1234567");
+
+        SSLServerSocketFactory serverSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocket serverSocket = null;
+
+        try {
+            serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(PORT);
+            System.out.println("Servidor Montado Correctamente");
+            while (true) {
+                SSLSocket peticion = (SSLSocket) serverSocket.accept();
+                ServerThread serverThread = new ServerThread(peticion);
+                serverThread.start();
+                System.out.println("Peticion delegada");
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Error al crear el Socket servidor: " + e.getMessage());
+        }
+    }
+
 }
--- src/main/java/Server/ServerThread.java
+++ src/main/java/Server/ServerThread.java
@@ -1,4 +1,93 @@
 package Server;
 
-public class ServerThread {
+import Models.Usuario;
+import dao.UsuarioDaoImpl;
+
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class ServerThread extends Thread {
+    SSLSocket peticion;
+    ObjectOutputStream salida;
+    ObjectInputStream entrada;
+
+    public ServerThread(SSLSocket peticion) {
+        this.peticion = peticion;
+        try {
+            salida = new ObjectOutputStream(peticion.getOutputStream());
+            entrada = new ObjectInputStream(peticion.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Error al crear entradas y salidas del hilo servidor: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+
+        URL persistenceXml = Thread.currentThread().getContextClassLoader().getResource("META-INF/persistence.xml");
+        System.out.println("persistence.xml encontrado en: " + persistenceXml);
+
+        try {
+            int peticion = (Integer) entrada.readObject();
+            salida.writeObject(peticion);
+            switch (peticion) {
+                case 100:
+                    byte[] contrasenia = (byte[]) entrada.readObject();
+                    String passField = (String) entrada.readObject();
+
+                    try {
+                        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+                        messageDigest.update(passField.getBytes());
+                        byte[] hashed = messageDigest.digest();
+
+                        if (Arrays.equals(hashed, contrasenia)) {
+                            salida.writeObject(true);
+                        } else salida.writeObject(false);
+                    } catch (NoSuchAlgorithmException e) {
+                        System.out.println("Error en la codificacion de la contraseña: " + e.getMessage());
+                        salida.writeObject(false);
+                    }
+
+
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al recibir el objeto: " + e.getMessage());
+            try {
+                salida.writeObject(-500);
+            } catch (IOException ex) {
+                System.out.println("Si esto falla ya salvame diosito");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al parcear el objeto: " + e.getMessage());
+        }
+    }
+
+
+    private KeyPair GenerarLLaves() {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048); // Tamaño de la clave de 2048 bits
+            keys = keyPairGenerator.genKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No se puede generar el RSA");
+        }
+
+        return keys;
+    }
 }
